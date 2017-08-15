7fdd6d0d5d96ee88b5ddb777b68abc71f902e1c1
Eddie Riggs <ace135adobe@gmail.com>
2017-08-15 20:08:14 +0200

    touches on scores

---
+++ Assets/scripts/showMillis.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class showMillis : MonoBehaviour {
+
+	private float time_elapsed;
+	private Text textClock;
+
+	// Use this for initialization
+	void Start () {
+		time_elapsed = 1000.0f;
+		textClock = GetComponent<Text> ();
+		enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (enabled) {
+			time_elapsed -= Time.deltaTime;
+			int decimal_part = (int)((time_elapsed - (int)time_elapsed) * 100);
+			string millis = decimal_part.ToString ().PadLeft (2, '0');
+			textClock.text = millis;
+		}
+	}
+}
---
+++ Assets/scripts/updateScore.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class updateScore : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// Mouse button
+		if (Input.GetKeyDown ("mouse 0")) {
+			if (touchInsideObject (Input.mousePosition)) {
+				updateScoreText ();
+
+			}
+		}
+
+		// Single touch
+		if (Input.touchCount > 0) {
+			Touch touch = Input.touches [0];
+			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled) {
+				if (touchInsideObject (touch.position)) {
+					updateScoreText ();
+				}
+			}
+		}
+
+		// Multi touch
+		/*foreach (Touch touch in Input.touches) {
+			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled) {
+				if (touchInsideObject (touch.position)) {
+					updateScoreText ();
+				}
+			}
+		}*/
+	}
+
+	private bool touchInsideObject(Vector2 p) {
+		Text textObject = GetComponent<Text> ();
+		return (p.x >= textObject.rectTransform.position.x - textObject.rectTransform.rect.width / 2
+			&& p.x <= textObject.rectTransform.position.x + textObject.rectTransform.rect.width / 2
+			&& p.y >= textObject.rectTransform.position.y - textObject.rectTransform.rect.height / 2
+			&& p.y <= textObject.rectTransform.position.y + textObject.rectTransform.rect.height / 2);
+	}
+
+	private void updateScoreText()
+	{
+		Text text = GetComponent<Text> ();
+		int score = int.Parse(text.text);
+		score += 1;
+		if (score >= 100)
+			score = 0;
+		text.text = score.ToString().PadLeft(2, '0');
+	}
+}
