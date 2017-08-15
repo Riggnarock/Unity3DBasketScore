b0faf7c1ff002f63e340791877eee2d29f4606d2
Eddie Riggs <ace135adobe@gmail.com>
2017-08-15 17:18:46 +0200

    labels for timers and scores

---
+++ Assets/scripts/timer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour {
+
+	float time_elapsed;
+	int minutes;
+	bool stopped;
+
+	// Text Object
+	private Text textClock;
+
+	// Use this for initialization
+	void Start () {
+		minutes = 10;
+		time_elapsed = minutes * 60;
+		stopped = false;
+
+		textClock = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!stopped) {
+			time_elapsed -= Time.deltaTime;
+			string minute = timeFormat ((int)(time_elapsed / 60));
+			string second = timeFormat ((int)(time_elapsed % 60));
+
+			textClock.text = minute + ":" + second;
+		}
+	}
+
+	/**
+	 * returns 2-digit time format from given number n
+	 */
+	private string timeFormat(int n) {
+		return n.ToString ().PadLeft (2, '0');
+	}
+}
